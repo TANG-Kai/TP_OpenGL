04846160f3b89da744912ea83fa2e0bc81f2609a
TANG Kai <kai.tang@telecom-paristech.fr>
2015-10-30 11:31:53 +0100

    get Main.cpp indented with qcreator

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,7 @@
 // ----------------------------------------------
-// Informatique Graphique 3D & Réalité Virtuelle.
+// Informatique Graphique 3D & Rï¿½alitï¿½ Virtuelle.
 // Travaux Pratiques
-// Introduction à OpenGL
+// Introduction ï¿½ OpenGL
 // Copyright (C) 2015 Tamy Boubekeur
 // All rights reserved.
 // ----------------------------------------------
@@ -35,133 +35,133 @@ static float camTargetY;
 static float camTargetZ;
 
 void polar2Cartesian (float phi, float theta, float d, float & x, float & y, float & z) {
-	x = d*sin (theta) * cos (phi);
+    x = d*sin (theta) * cos (phi);
     y = d*cos (theta);
-	z = d*sin (theta) * sin (phi);
+    z = d*sin (theta) * sin (phi);
 }
 
 void printUsage () {
-	std::cerr << std::endl // send a line break to the standard error output
-		 << appTitle << std::endl
-         << "Author : Tamy Boubekeur" << std::endl << std::endl
-         << "Usage : ./main [<file.off>]" << std::endl
-         << "Cammandes clavier :" << std::endl 
-         << "------------------" << std::endl
-         << " ?: Print help" << std::endl
-		 << " w: Toggle wireframe mode" << std::endl
-         << " <drag>+<left button>: rotate model" << std::endl 
-         << " <drag>+<right button>: move model" << std::endl
-         << " <drag>+<middle button>: zoom" << std::endl
-         << " q, <esc>: Quit" << std::endl << std::endl; 
+    std::cerr << std::endl // send a line break to the standard error output
+              << appTitle << std::endl
+              << "Author : Tamy Boubekeur" << std::endl << std::endl
+              << "Usage : ./main [<file.off>]" << std::endl
+              << "Cammandes clavier :" << std::endl
+              << "------------------" << std::endl
+              << " ?: Print help" << std::endl
+              << " w: Toggle wireframe mode" << std::endl
+              << " <drag>+<left button>: rotate model" << std::endl
+              << " <drag>+<right button>: move model" << std::endl
+              << " <drag>+<middle button>: zoom" << std::endl
+              << " q, <esc>: Quit" << std::endl << std::endl;
 }
 
-void init () {  
-	// OpenGL initialization
+void init () {
+    // OpenGL initialization
     glCullFace (GL_BACK);     // Specifies the faces to cull (here the ones pointing away from the camera)
     glEnable (GL_CULL_FACE); // Enables face culling (based on the orientation defined by the CW/CCW enumeration).
     glDepthFunc (GL_LESS); // Specify the depth test for the z-buffer
     glEnable (GL_DEPTH_TEST); // Enable the z-buffer in the rasterization
-	glLineWidth (2.0); // Set the width of edges in GL_LINE polygon mode
+    glLineWidth (2.0); // Set the width of edges in GL_LINE polygon mode
     glClearColor (0.0f, 0.0f, 0.0f, 1.0f); // Background color
-	
-	// Camera initialization
-	fovAngle = 45.f;
-	nearPlane = 0.01;
-	farPlane = 10.0;
-	camPhi = M_PI/2.0;
-	camTheta = M_PI/2.0;
-	camDist2Target = 5.0;
-	camTargetX = 0.0;
-	camTargetY = 0.0;
-	camTargetZ = 0.0;
+
+    // Camera initialization
+    fovAngle = 45.f;
+    nearPlane = 0.01;
+    farPlane = 10.0;
+    camPhi = M_PI/2.0;
+    camTheta = M_PI/2.0;
+    camDist2Target = 5.0;
+    camTargetX = 0.0;
+    camTargetY = 0.0;
+    camTargetZ = 0.0;
 }
 
 void setupCamera () {
-	glMatrixMode (GL_PROJECTION); // Set the projection matrix as current. All upcoming matrix manipulations will affect it.
-	glLoadIdentity ();
-	gluPerspective (fovAngle, aspectRatio, nearPlane, farPlane); // Set the current projection matrix with the camera intrinsics
-	glMatrixMode (GL_MODELVIEW); // Set the modelview matrix as current. All upcoming matrix manipulations will affect it.
-	glLoadIdentity ();
-	float camPosX, camPosY, camPosZ;
-	polar2Cartesian (camPhi, camTheta, camDist2Target, camPosX, camPosY, camPosZ);
-	camPosX += camTargetX;
-	camPosY += camTargetY;
-	camPosZ += camTargetZ;
-	gluLookAt (camPosX, camPosY, camPosZ, camTargetX, camTargetY, camTargetZ, 0.0, 1.0, 0.0); // Set up the current modelview matrix with camera transform
+    glMatrixMode (GL_PROJECTION); // Set the projection matrix as current. All upcoming matrix manipulations will affect it.
+    glLoadIdentity ();
+    gluPerspective (fovAngle, aspectRatio, nearPlane, farPlane); // Set the current projection matrix with the camera intrinsics
+    glMatrixMode (GL_MODELVIEW); // Set the modelview matrix as current. All upcoming matrix manipulations will affect it.
+    glLoadIdentity ();
+    float camPosX, camPosY, camPosZ;
+    polar2Cartesian (camPhi, camTheta, camDist2Target, camPosX, camPosY, camPosZ);
+    camPosX += camTargetX;
+    camPosY += camTargetY;
+    camPosZ += camTargetZ;
+    gluLookAt (camPosX, camPosY, camPosZ, camTargetX, camTargetY, camTargetZ, 0.0, 1.0, 0.0); // Set up the current modelview matrix with camera transform
 }
 
 void reshape (int w, int h) {
     screenWidth = w;
-	screenHeight = h;
-	aspectRatio = static_cast<float>(w)/static_cast<float>(h);
-	glViewport (0, 0, (GLint)w, (GLint)h); // Dimension of the drawing region in the window
-	setupCamera ();
+    screenHeight = h;
+    aspectRatio = static_cast<float>(w)/static_cast<float>(h);
+    glViewport (0, 0, (GLint)w, (GLint)h); // Dimension of the drawing region in the window
+    setupCamera ();
 }
 
 void glSphere(float x,float y, float z, float radius){
-	int const num_of_points = 1000;
-	double const r = radius;
-	double const delta = M_PI *1.0/ num_of_points;
-	glBegin(GL_TRIANGLES);
-	for(double i = 0;i< M_PI;i+= delta){
-		for(double j = 0;j< M_PI;j+= delta){
-  		glColor3f(r*cos(i)*sin(j),r*cos(i)*cos(j),r*sin(i));
-  		glVertex3f(r*cos(i)*sin(j),r*cos(i)*cos(j),r*sin(i));
-		
-			i +=delta;
-  		glColor3f(r*cos(i)*sin(j),r*cos(i)*cos(j),r*sin(i));
-  		glVertex3f(r*cos(i)*sin(j),r*cos(i)*cos(j),r*sin(i));
-			i-= delta;
+    int const num_of_points = 1000;
+    double const r = radius;
+    double const delta = M_PI *1.0/ num_of_points;
+    glBegin(GL_TRIANGLES);
+    for(double i = 0;i< M_PI;i+= delta){
+        for(double j = 0;j< M_PI;j+= delta){
+            glColor3f(r*cos(i)*sin(j),r*cos(i)*cos(j),r*sin(i));
+            glVertex3f(r*cos(i)*sin(j),r*cos(i)*cos(j),r*sin(i));
 
-			j +=delta;
-  		glColor3f(r*cos(i)*sin(j),r*cos(i)*cos(j),r*sin(i));
-  		glVertex3f(r*cos(i)*sin(j),r*cos(i)*cos(j),r*sin(i));
-			j-= delta;
+            i +=delta;
+            glColor3f(r*cos(i)*sin(j),r*cos(i)*cos(j),r*sin(i));
+            glVertex3f(r*cos(i)*sin(j),r*cos(i)*cos(j),r*sin(i));
+            i-= delta;
 
-			i +=delta;
-  		glColor3f(r*cos(i)*sin(j),r*cos(i)*cos(j),r*sin(i));
-  		glVertex3f(r*cos(i)*sin(j),r*cos(i)*cos(j),r*sin(i));
-			i-= delta;
+            j +=delta;
+            glColor3f(r*cos(i)*sin(j),r*cos(i)*cos(j),r*sin(i));
+            glVertex3f(r*cos(i)*sin(j),r*cos(i)*cos(j),r*sin(i));
+            j-= delta;
 
-			j +=delta;
-  		glColor3f(r*cos(i)*sin(j),r*cos(i)*cos(j),r*sin(i));
-  		glVertex3f(r*cos(i)*sin(j),r*cos(i)*cos(j),r*sin(i));
-			j-= delta;
+            i +=delta;
+            glColor3f(r*cos(i)*sin(j),r*cos(i)*cos(j),r*sin(i));
+            glVertex3f(r*cos(i)*sin(j),r*cos(i)*cos(j),r*sin(i));
+            i-= delta;
 
-			j +=delta;i+=delta;
-  		glColor3f(r*cos(i)*sin(j),r*cos(i)*cos(j),r*sin(i));
-  		glVertex3f(r*cos(i)*sin(j),r*cos(i)*cos(j),r*sin(i));
-			j-= delta;i-=delta;
-		}
-	}
-	glEnd();
-	// set an offset
-	glMatrixMode(GL_MODELVIEW);
-	glPushMatrix();
-	glTranslatef(x,y,z);
-	glPopMatrix();
+            j +=delta;
+            glColor3f(r*cos(i)*sin(j),r*cos(i)*cos(j),r*sin(i));
+            glVertex3f(r*cos(i)*sin(j),r*cos(i)*cos(j),r*sin(i));
+            j-= delta;
+
+            j +=delta;i+=delta;
+            glColor3f(r*cos(i)*sin(j),r*cos(i)*cos(j),r*sin(i));
+            glVertex3f(r*cos(i)*sin(j),r*cos(i)*cos(j),r*sin(i));
+            j-= delta;i-=delta;
+        }
+    }
+    glEnd();
+    // set an offset
+    glMatrixMode(GL_MODELVIEW);
+    glPushMatrix();
+    glTranslatef(x,y,z);
+    glPopMatrix();
 }
-void display () {  
-    setupCamera ();   
+void display () {
+    setupCamera ();
     glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Erase the color and z buffers.
 
-	// Put your drawing code (glBegin, glVertex, glCallList, glDrawArray, etc) here
-	glSphere(0,0,0,1);
-  glFlush (); // Ensures any previous OpenGL call has been executed
-  glutSwapBuffers ();  // swap the render buffer and the displayed (screen) one
+    // Put your drawing code (glBegin, glVertex, glCallList, glDrawArray, etc) here
+    glSphere(0,0,0,1);
+    glFlush (); // Ensures any previous OpenGL call has been executed
+    glutSwapBuffers ();  // swap the render buffer and the displayed (screen) one
 }
 void keyboard (unsigned char keyPressed, int x, int y) {
     switch (keyPressed) {
-	case 'w':
-		GLint mode[2];
-		glGetIntegerv (GL_POLYGON_MODE, mode);
-		glPolygonMode (GL_FRONT_AND_BACK, mode[1] ==  GL_FILL ? GL_LINE : GL_FILL);
+    case 'w':
+        GLint mode[2];
+        glGetIntegerv (GL_POLYGON_MODE, mode);
+        glPolygonMode (GL_FRONT_AND_BACK, mode[1] ==  GL_FILL ? GL_LINE : GL_FILL);
         break;
     case 'q':
     case 27:
         exit (0);
         break;
-     default:
+    default:
         printUsage ();
         break;
     }
@@ -186,13 +186,12 @@ int main (int argc, char ** argv) {
     window = glutCreateWindow (appTitle.c_str ()); // create the window
     init (); // Your initialization code (OpenGL states, geometry, material, lights, etc)
     glutReshapeFunc (reshape); // Callback function executed whenever glut need to setup the projection matrix
-	glutDisplayFunc (display); // Callback function executed when the window app need to be redrawn
+    glutDisplayFunc (display); // Callback function executed when the window app need to be redrawn
     glutKeyboardFunc (keyboard); // Callback function executed when the keyboard is used
-    glutMouseFunc (mouse); // Callback function executed when a mouse button is clicked 
-	glutMotionFunc (motion); // Callback function executed when the mouse move
-	glutIdleFunc (idle); // Callback function executed continuously when no other event happens (good for background procesing or animation for instance).
-    printUsage (); // By default, display the usage help of the program   
+    glutMouseFunc (mouse); // Callback function executed when a mouse button is clicked
+    glutMotionFunc (motion); // Callback function executed when the mouse move
+    glutIdleFunc (idle); // Callback function executed continuously when no other event happens (good for background procesing or animation for instance).
+    printUsage (); // By default, display the usage help of the program
     glutMainLoop ();
     return 0;
 }
-
